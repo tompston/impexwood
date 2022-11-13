52ad16a9e02b765263c941f3e2e010755523164d
tompston <tompston@protonmail.com>
2022-11-14 00:49:43 +0200

    removed aos, changed top img

--- src/main.js
+++ src/main.js
@@ -4,5 +4,9 @@ import "./css/index.css"
 import "./css/other.css"
 import "./css/media.css"
 
-// import "./js/gsap_animations"
+// cdn vue
 import "./js/vue"
+
+// cdn aos
+// import "./js/aos/aos.js"
+// import "./js/aos/aos.css"
