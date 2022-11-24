37b2eadcf00679c10d1f72a623f4240cd890b87a
tompston <tompston@protonmail.com>
2022-11-24 13:49:07 +0200

    added build dir and new product details

--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -39,28 +39,48 @@ export default function ProductSection() {
                         </div>
                     </div>
                 } />
+
+            <ProductCard
+                name="Pallet wood cut to lengths"
+                img_src="/img/03.jpg"
+                content={
+                    <div className="flex flex-col gap-2">
+                        Made out of first grade softwood boards.
+                        We offer a variety of sizes and dimensions for
+                        pallet industry planed 4 sides ( cut to lengths of
+                        EUR pallet sizes and many others )
+
+                        <div>
+                            Our main size are within a range of
+                            <ul>
+                                {[
+                                    "22x98x800/1000/1200",
+                                    "22x145x800/1200",
+                                    "16x100x800/1000/1200",
+                                    "16x75x800/1000/1200/1300",
+                                ].map((x, i) => (
+                                    <li key={i} className="mb-1.5">{x}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                } />
             <ProductCard
                 name="Finger jointed knotfree pine and spruce boards"
                 img_src="/img/02.jpg"
                 content={
                     <div className="flex flex-col gap-2">
                         <ul>
-                            {product_dims.map((x, i) => (
+                            {[
+                                "32x75/100/125/150/200mm",
+                                "25x75/100/125/150mm",
+                                "19x75/100/125/150mm",
+                                "16x75/100/125mm",
+                                "22x100",
+                            ].map((x, i) => (
                                 <li key={i} className="mb-1.5">{x}</li>
                             ))}
                         </ul>
-                    </div>
-                } />
-            <ProductCard
-                name="Pallet wood cut to lengths"
-                img_src="/img/03.jpg"
-                content={
-                    <div className="flex flex-col gap-2">
-                        {/* <ul>
-                            {product_dims.map((x, i) => (
-                                <li key={i} className="mb-1.5">{x}</li>
-                            ))}
-                        </ul> */}
                     </div>
                 } />
         </div>
@@ -87,16 +107,16 @@ function ProductCard({ name, img_src, content }: ProductProps) {
             <div className="p-4">
                 <div className="min-h-[50px]"
                 ><h6 className="fw-600 leading-none mb-2">{name}</h6></div>
-                <div className="opacity-70 fs-8">{content}</div>
+                <div className="opacity-70 fs-9">{content}</div>
             </div>
         </div>
     )
 }
 
-const product_dims: string[] = [
-    "32x75/100/125/150/200mm",
-    "25x75/100/125/150mm",
-    "19x75/100/125/150mm",
-    "16x75/100/125mm",
-    "22x100",
-]
+// const product_dims: string[] = [
+//     "32x75/100/125/150/200mm",
+//     "25x75/100/125/150mm",
+//     "19x75/100/125/150mm",
+//     "16x75/100/125mm",
+//     "22x100",
+// ]
